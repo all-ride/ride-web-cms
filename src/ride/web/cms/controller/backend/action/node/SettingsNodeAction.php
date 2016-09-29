2e6bae0ce31b6f720cf0c2c9f9880a8f9b2732c5
kristof <kristof@statik.be>
2016-09-29 09:51:09 +0200

    Added settingsNodeAction to show in node actions tabs

---
+++ src/ride/web/cms/controller/backend/action/node/SettingsNodeAction.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace ride\web\cms\controller\backend\action\node;
+
+use ride\library\cms\node\Node;
+
+use ride\web\cms\Cms;
+
+/**
+ * Controller of the go node action
+ */
+class SettingsNodeAction extends AbstractNodeAction {
+
+    /**
+     * Name of this action
+     * @var string
+     */
+    const NAME = 'settings';
+
+    /**
+     * Route of this action
+     * @var string
+     */
+    const ROUTE = 'cms.page.edit';
+
+    /**
+     * Instance of the CMS facade
+     * @var \ride\web\cms\Cms
+     */
+    protected $cms;
+
+    /**
+     * Sets the CMS
+     * @param \ride\web\cms\Cms $cms
+     * @return null
+     */
+    public function setCms(Cms $cms) {
+        $this->cms = $cms;
+    }
+
+    /**
+     * Checks if this action is available for the node
+     * @param \ride\library\cms\node\Node $node
+     * @return boolean True if available
+     */
+    public function isAvailableForNode(Node $node) {
+        if (!$node->getParent()) {
+            return true;
+        }
+
+        $nodeType = $this->cms->getNodeType($node);
+
+        return $nodeType->getFrontendCallback() ? true : false;
+    }
+
+    /**
+     * Perform the go node action
+     * @return null
+     */
+    public function indexAction($locale, $site, $revision, $node) {
+        if (!$this->cms->resolveNode($site, $revision, $node)) {
+            return;
+        }
+
+        if ($node->getType() === 'site') {
+            $url = $node->getBaseUrl($locale);
+            if (!$url) {
+                $url = $this->request->getBaseScript();
+            }
+        } else {
+            $url = $this->getUrl('cms.front.' . $site->getId() . '.' . $node->getId() . '.' . $locale);
+        }
+
+        $this->response->setRedirect($url);
+    }
+
+}
